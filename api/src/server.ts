6c363526b7a4cea5e2d2fb9baceddca74b934669
Daniel Silva <daniel.silva.aoop@elo.com.br>
2022-09-29 22:22:46 -0300

    Salvar progresso

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import categoriesRoutes from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (req, res) => res.json({
-  message: "hi mom!",
-}));
+app.use(express.json());
 
-app.listen(3333, () => console.log("server is running"));
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333);
